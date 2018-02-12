b4fc14af91f48a2973bbb3a81dbc7000744e5a29
dabliuw22 <wleylop22@gmail.com>
2018-02-12 15:55:09 -0500

    Agregando interface e implementación de servicio de seguridad SegurityService

---
+++ src/main/java/com/leysoft/app/services/imple/SecurityServiceImp.java
@@ -0,0 +1,68 @@
+package com.leysoft.app.services.imple;
+
+import java.util.UUID;
+
+import javax.mail.MessagingException;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import com.leysoft.app.entitys.User;
+import com.leysoft.app.services.inter.MailService;
+import com.leysoft.app.services.inter.SecurityService;
+import com.leysoft.app.services.inter.UserService;
+import com.leysoft.app.utilitys.SecurityUtil;
+import com.leysoft.app.utilitys.converters.inter.Converter;
+import com.leysoft.app.utilitys.models.UserModel;
+
+@Service
+public class SecurityServiceImp implements SecurityService {
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	private Converter<User, UserModel> userConverter;
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private MailService mailService;
+	
+	@Override
+	public void authentication(User user) {
+		UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUsername());
+		Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(), userDetails.getAuthorities());
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+	}
+	
+	@Override
+	public void saveUser(UserModel user, HttpServletRequest request) throws MailException, MessagingException {
+		User newUser = userConverter.modelToEntity(user);
+		String password = SecurityUtil.randomPassword();
+		userService.save(newUser, password);
+		String token = UUID.randomUUID().toString();
+		userService.createPasswordResetTokenForUser(newUser, token);
+		String url = "http://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
+		mailService.sendEmailHtml(url, request.getLocale(), token, newUser, password);
+	}
+	
+	@Override
+	public void resetPasswordUser(User user, HttpServletRequest request) throws MailException, MessagingException {
+		String password = SecurityUtil.randomPassword();
+		user.setPassword(SecurityUtil.passwordEncoder().encode(password));
+		userService.update(user);
+		String token = UUID.randomUUID().toString();
+		userService.createPasswordResetTokenForUser(user, token);
+		String url = "http://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
+		mailService.sendEmailHtml(url, request.getLocale(), token, user, password);
+	}
+}
---
+++ src/main/java/com/leysoft/app/services/inter/SecurityService.java
@@ -0,0 +1,18 @@
+package com.leysoft.app.services.inter;
+
+import javax.mail.MessagingException;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.mail.MailException;
+
+import com.leysoft.app.entitys.User;
+import com.leysoft.app.utilitys.models.UserModel;
+
+public interface SecurityService {
+	
+	public void authentication(User user);
+	
+	public void saveUser(UserModel user, HttpServletRequest request) throws MailException, MessagingException;
+	
+	public void resetPasswordUser(User user, HttpServletRequest request) throws MailException, MessagingException;
+}
