3f67be01a2a8829fd55989f8ee5d19a4784dfe44
dabliuw22 <wleylop22@gmail.com>
2018-02-12 21:37:51 -0500

    Agregando entidad Book

---
+++ src/main/java/com/leysoft/app/entitys/Book.java
@@ -0,0 +1,185 @@
+package com.leysoft.app.entitys;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+@Entity @Table(name = "books")
+public class Book implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String nombre;
+	
+	@Column
+	private String autor;
+	
+	@Lob
+	@Column
+	private String descripcion;
+	
+	@Column(name = "fecha_publicacion")
+	private Date fechaPublicacion;
+	
+	@Column
+	private String lenguaje;
+	
+	@Column
+	private String categoria;
+	
+	@Column(name = "numero_paginas")
+	private Integer numeroPaginas;
+	
+	@Column
+	private Double precio;
+	
+	@Column(name = "precio_envio")
+	private Double precioEnvio;
+	
+	@Column
+	private Integer stock;
+	
+	@Column
+	private boolean activo;
+
+	public Book() {
+		this.activo = true;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Date getFechaPublicacion() {
+		return fechaPublicacion;
+	}
+
+	public void setFechaPublicacion(Date fechaPublicacion) {
+		this.fechaPublicacion = fechaPublicacion;
+	}
+
+	public String getLenguaje() {
+		return lenguaje;
+	}
+
+	public void setLenguaje(String lenguaje) {
+		this.lenguaje = lenguaje;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public Integer getNumeroPaginas() {
+		return numeroPaginas;
+	}
+
+	public void setNumeroPaginas(Integer numeroPaginas) {
+		this.numeroPaginas = numeroPaginas;
+	}
+
+	public Double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+
+	public Double getPrecioEnvio() {
+		return precioEnvio;
+	}
+
+	public void setPrecioEnvio(Double precioEnvio) {
+		this.precioEnvio = precioEnvio;
+	}
+
+	public Integer getStock() {
+		return stock;
+	}
+
+	public void setStock(Integer stock) {
+		this.stock = stock;
+	}
+
+	public boolean isActivo() {
+		return activo;
+	}
+
+	public void setActivo(boolean activo) {
+		this.activo = activo;
+	}
+
+	@Override
+	public String toString() {
+		return nombre;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
