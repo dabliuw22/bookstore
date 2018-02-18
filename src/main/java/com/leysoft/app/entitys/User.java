5456997ee1d7a7b955c8f1d8f4970476e402a211
dabliuw22 <wleylop22@gmail.com>
2018-02-17 21:05:58 -0500

    Cambiando configuración fetch de tipo EAGER a tipo LAZY

--- src/main/java/com/leysoft/app/entitys/User.java
+++ src/main/java/com/leysoft/app/entitys/User.java
@@ -8,7 +8,6 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -53,7 +52,7 @@ public class User implements Serializable, UserDetails {
 	@Column
 	private boolean activo;
 
-	@ManyToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST}, fetch = FetchType.EAGER)
+	@ManyToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
 	@JoinTable(name = "roles_por_usuario", joinColumns = {@JoinColumn(name = "usuario_id", nullable = false)},
 		inverseJoinColumns = {@JoinColumn(name = "rol_id", nullable = false)})
 	private List<Role> roles;
