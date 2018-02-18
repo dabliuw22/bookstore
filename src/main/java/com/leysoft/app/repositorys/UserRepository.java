7e6487745b2d695d5852b189c19f0f739e80456a
dabliuw22 <wleylop22@gmail.com>
2018-02-17 21:07:31 -0500

    Agregando métodos con consulta personalizada para obtener User con el uso de join fetch y @Query

--- src/main/java/com/leysoft/app/repositorys/UserRepository.java
+++ src/main/java/com/leysoft/app/repositorys/UserRepository.java
@@ -1,5 +1,6 @@
 package com.leysoft.app.repositorys;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.leysoft.app.entitys.User;
@@ -8,5 +9,11 @@ public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 	
 	public User findByUsername(String username);
 	
+	@Query("select u from User as u left join fetch u.roles as r where u.username = ?1")
+	public User findByUsernameJoinFetch(String username);
+	
 	public User findByEmail(String email);
+	
+	@Query("select u from User as u left join fetch u.roles as r where u.email = ?1")
+	public User findByEmailJoinFetch(String email);
 }
