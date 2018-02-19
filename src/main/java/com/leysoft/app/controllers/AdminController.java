41afacaeb1a7745e11165c66c6f6c91f6b3cbe01
dabliuw22 <wleylop22@gmail.com>
2018-02-18 19:56:48 -0500

    Modificando AdminController

--- src/main/java/com/leysoft/app/controllers/AdminController.java
+++ src/main/java/com/leysoft/app/controllers/AdminController.java
@@ -1,13 +1,45 @@
 package com.leysoft.app.controllers;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.leysoft.app.entitys.Book;
+import com.leysoft.app.utilitys.validators.BookValidator;
 
 @Controller
+@RequestMapping(value = "/admin")
 public class AdminController {
 
-	@GetMapping(value = "/admin")
+	@Autowired
+	private BookValidator bookValidator;
+	
+	@GetMapping(value = "")
 	public String dashboard() {
 		return "admin/dashboard";
+	}
+	
+	@GetMapping(value = "/book/add")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "admin/book/create";
+	}
+	
+	@PostMapping(value = "/book/add")
+	public String addBook(@Valid @ModelAttribute("book") Book book, BindingResult errors, Model model) {
+		bookValidator.validate(book, errors);
+		if(!errors.hasErrors()) {
+			System.out.println(book.getFile().getContentType());
+			return "redirect:/admin";
+		}
+		model.addAttribute("book", book);
+		return "admin/book/create";
 	}
 }
