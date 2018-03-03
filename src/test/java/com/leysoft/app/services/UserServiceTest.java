9df1924b24cf125a85a6301368317eb1cb81d259
dabliuw22 <wleylop22@gmail.com>
2018-03-03 15:36:19 -0500

    Agregando método throws Exeption a setUp()

--- src/test/java/com/leysoft/app/services/UserServiceTest.java
+++ src/test/java/com/leysoft/app/services/UserServiceTest.java
@@ -29,7 +29,7 @@ public class UserServiceTest {
 	private UserServiceImp userService;
 	
 	@Before
-	public void setUp() {
+	public void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
 	}
 
