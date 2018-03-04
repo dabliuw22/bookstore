c07b1858bf9c2ae4bb0e9412a59216bede0c1473
dabliuw22 <wleylop22@gmail.com>
2018-03-03 19:26:30 -0500

    Agregando @Bean para RoleService y UploadFileService

--- src/main/java/com/leysoft/app/BookstoreApplication.java
+++ src/main/java/com/leysoft/app/BookstoreApplication.java
@@ -4,12 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.leysoft.app.entitys.Role;
+import com.leysoft.app.services.imple.RoleServiceImp;
+import com.leysoft.app.services.imple.UploadFileServiceImp;
 import com.leysoft.app.services.inter.RoleService;
 import com.leysoft.app.services.inter.UploadFileService;
+
+
+/**
+ * Esta clase define la configuración de arranque para el proyecto.
+ * 
+ * @author will
+ */
 
 @EnableScheduling
 @EnableAsync
@@ -22,10 +32,21 @@ public class BookstoreApplication implements CommandLineRunner {
 	@Autowired
 	private RoleService roleService;
 	
+	/**
+	 * Método main para el arranque de la app spring-boot.
+	 * 
+	 * @param args array de String que recibe el método statico.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	/**
+	 * Método que se debe a la implementación de la interface CommandLineRunner,
+	 * el cual se ejecutara después que el contexto se construya.
+	 * 
+	 * @param arg0 String que recibe el método.
+	 */
 	@Override
 	public void run(String... arg0) throws Exception {
 		uploadFileService.deleteAll();
@@ -37,5 +58,25 @@ public class BookstoreApplication implements CommandLineRunner {
 		Role roleAdmin = new Role();
 		roleAdmin.setNombre("ROLE_ADMIN");
 		roleService.save(roleAdmin);
+	}
+	
+	/**
+	 * Bean para agregar UploadFileService al contexto de Spring.
+	 * 
+	 * @return La implementación UploadFileServiceImp.
+	 */
+	@Bean
+	public UploadFileService uploadFileService() {
+		return new UploadFileServiceImp();
+	}
+	
+	/**
+	 * Bean para agregar RoleService al contexto de Spring.
+	 * 
+	 * @return La implementación RoleServiceImp.
+	 */
+	@Bean
+	public RoleService roleService() {
+		return new RoleServiceImp();
 	}
 }
